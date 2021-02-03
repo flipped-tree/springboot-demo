36551f279a4e0505b0b44d96045e0fd996b5fdb5
xingce <xingce@wejoydata.com>
2021-02-03 22:28:43 +0800

    commit

--- src/main/java/com/example/springboot/retrofit/CommonResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.retrofit;
-
-/**
- * @author xingce
- * @date 2020/12/17 12:10
- */
-public class CommonResponse<T> {
-}
--- src/main/java/com/example/springboot/retrofit/RetrofitResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.retrofit;
-
-/**
- * @author xingce
- * @date 2020/12/17 12:09
- */
-public class RetrofitResponse {
-}
