042eb24bf231e57b7b9610bd43c87f90e00696cd
xingce <xingce@wejoydata.com>
2021-04-21 16:40:22 +0800

    修改代码

--- src/main/java/com/example/practice/algorithm/StringSplit.java
+++ src/main/java/com/example/practice/algorithm/StringSplit.java
@@ -1,6 +1,6 @@
 package com.example.practice.algorithm;
 
-import jodd.util.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,7 +8,7 @@ import java.util.Arrays;
 /**
  * 手写字符串分割函数
  *
- * @author xingce14590
+ * @author xingce
  * @date 2021/3/22 19:46
  */
 public class StringSplit {
@@ -21,13 +21,13 @@ public class StringSplit {
         int limit;
         String splitStr;
         ArrayList<String> result = new ArrayList<>();
-        while (str.contains(regx)) {// 设置要截取的位置
+        while (str.contains(regx)) {
             limit = str.indexOf(regx);
-            splitStr = str.substring(0, limit);// 设置截取后的字符串
+            splitStr = str.substring(0, limit);
             str = str.substring(limit + regx.length());
             result.add(splitStr);
         }
-        if (!StringUtil.isBlank(str)) {
+        if (StringUtils.isNotBlank(str)) {
             result.add(str);
         }
         String[] strings = new String[result.size()];
