b31987843789c3a5b034834833ac2c99564a6f29
cexing <cexing@trip.com>
2021-05-18 17:18:39 +0800

    add:weak reference test

---
+++ src/main/java/com/example/threadLocal/WeakReferenceDemo.java
@@ -0,0 +1,25 @@
+package com.example.threadLocal;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * 弱引用demo
+ *
+ * @author xingce
+ */
+public class WeakReferenceDemo {
+    public static void main(String[] args) {
+        Object object = new Object();
+        // 弱引用
+        WeakReference<Object> weakReference = new WeakReference<>(object);
+        // object和弱引用指向同一个对象
+        System.out.println(object);
+        System.out.println(weakReference.get());
+        // 手动回收object，object只剩下弱引用
+        object = null;
+        // 手动唤醒gc
+        System.gc();
+        System.out.println(object);
+        System.out.println(weakReference.get());
+    }
+}
