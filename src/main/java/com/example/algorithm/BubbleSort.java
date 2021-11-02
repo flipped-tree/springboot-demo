1bb9f3a70550b73ce79e14c906f8f739d1694e91
Cexing <cexing@trip.com>
2021-11-02 15:17:33 +0800

    优化冒泡排序

--- src/main/java/com/example/algorithm/BubbleSort.java
+++ src/main/java/com/example/algorithm/BubbleSort.java
@@ -11,20 +11,33 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] s = {4, 3, 2, 1};
-        sort(s);
+        int[] ints = {4, 3, 5, 6};
+        sort(ints);
+        System.out.println(Arrays.toString(ints));
     }
 
     private static void sort(int[] ints) {
-        for (int i = 0, length = ints.length; i < length; i++) {
-            for (int j = 0; j < length - i - 1; j++) {
+        for (int i = 0; i < ints.length - 1; i++) {
+            // 判断是否已交换，不交换即表示已排序结束
+            boolean swapped = false;
+            for (int j = 0; j < ints.length - 1 - i; j++) {
                 if (ints[j] > ints[j + 1]) {
-                    int temp = ints[j + 1];
-                    ints[j + 1] = ints[j];
-                    ints[j] = temp;
+                    swap(ints, j);
+                    swapped = true;
                 }
             }
-            System.out.println(Arrays.toString(ints));
+            if (!swapped) {
+                break;
+            }
         }
+    }
+
+    /**
+     * 交换位置
+     */
+    private static void swap(int[] ints, int j) {
+        int temp = ints[j + 1];
+        ints[j + 1] = ints[j];
+        ints[j] = temp;
     }
 }
