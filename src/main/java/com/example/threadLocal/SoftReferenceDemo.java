263bf7cec91ae68950f42bd7430269cdbe1fd26f
Ce Xing （邢策） <cexing@trip.com>
2021-12-28 19:28:07 +0800

    添加demo

---
+++ src/main/java/com/example/threadLocal/SoftReferenceDemo.java
@@ -0,0 +1,23 @@
+package com.example.threadLocal;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * 软引用，JVM gc时内存不足回收对象
+ *
+ * @author xingce
+ * @date 2021/12/28 19:18
+ */
+public class SoftReferenceDemo {
+
+    public static void main(String[] args) {
+        SoftReference<byte[]> softReference = new SoftReference<>(new byte[1024 * 1024 * 10]);
+        System.out.println(softReference.get());
+        System.gc();
+        System.out.println(softReference.get());
+
+        byte[] bytes = new byte[1024 * 1024 * 10];
+        System.out.println(softReference.get());
+    }
+
+}
--- src/main/java/com/example/threadLocal/WeakReferenceDemo.java
+++ src/main/java/com/example/threadLocal/WeakReferenceDemo.java
@@ -3,7 +3,7 @@ package com.example.threadLocal;
 import java.lang.ref.WeakReference;
 
 /**
- * 弱引用demo
+ * 弱引用，JVM gc时直接回收对象
  *
  * @author xingce
  */
