4adfc9dbbb1d09b16ffec8f84db74184d687c13d
Ce Xing （邢策） <cexing@trip.com>
2022-01-20 19:14:59 +0800

    修改线程池demo

--- src/main/java/com/example/threadpool/ThreadPoolExecutorDemo.java
+++ src/main/java/com/example/threadpool/ThreadPoolExecutorDemo.java
@@ -4,18 +4,36 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * @author xingce
- * @date 2021/3/6 13:20
  * 判断线程池是否结束
  * 线程池拒绝策略后无法正常关闭
+ * <p>
+ * 线程池通过32位长度二进制进行线程状态和数量的判断
+ * 高3位表示状态 低29位标识数量
+ *
+ * @author xingce
+ * @date 2021/3/6 13:20
  */
-public class ThreadPoolTest {
+public class ThreadPoolExecutorDemo {
+
     public static void main(String[] args) {
+        /*
+         * @corePoolSize 线程池中一直存在的线程数，即使线程处于空闲状态，除非设置 allowCoreThreadTimeOut
+         * @maximumPooSize 线程池可允许的最大线程数
+         * @keepAliveTime 线程数大于核心线程数时，这是多余的空闲线程在中止之前等待新任务的最大时长
+         * @unit keepALiveTime的时间单位
+         * @workQueue 在任务之前之前保存任务的队列，这个队列只保存通过execute方法提交的Runnable任务
+         * @threadFactory 线程工厂，生成线程
+         * @handler executor由于线程临界和队列达到最大容量异常而触发的处理器
+         *      AbortPolicy 直接抛弃，抛出异常
+         *      CallerRunsPolicy 用调用者的线程执行任务
+         *      DisCardOldestPolicy 抛弃队列中最久的任务
+         *      DiscardPolicy 抛弃当前任务
+         */
         ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(2)
-                , new NamedThreadFactory("test-pool"), (r, executor1) -> {
+                , new NamedThreadFactory("test-pool"), (r, poolExecutor) -> {
             try {
                 System.out.println("重新放进线程池");
-                executor1.getQueue().put(r);
+                poolExecutor.getQueue().put(r);
             } catch (InterruptedException e) {
             }
         });
