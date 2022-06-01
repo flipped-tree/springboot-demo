e02cea5c2b4e4290a8134bf6087f45aa7d6a6203
Ce Xing （邢策） <cexing@trip.com>
2022-06-01 14:24:56 +0800

    test

--- src/main/java/com/example/springboot/SpringbootDemoApplication.java
+++ src/main/java/com/example/springboot/SpringbootDemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author xingce
+ *
+ * 使用非内置tomcat 继承 SpringBootServletInitializer，实现confirure方法
  */
 @SpringBootApplication(scanBasePackages = "com.example.springboot.*")
 public class SpringbootDemoApplication {
