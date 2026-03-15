be212fa822fe137385fd046973e59c80edf549b9
xc <271541760@qq.com>
2026-03-15 15:59:57 +0800

    bugfix

--- src/main/java/com/example/algorithm/leetcode/Code_19.java
+++ src/main/java/com/example/algorithm/leetcode/Code_19.java
@@ -3,13 +3,31 @@ package com.example.algorithm.leetcode;
 public class Code_19 {
     public static void main(String[] args) {
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        ListNode node = removeNode(head, 0);
+        ListNode node = removeNode(head, 1);
         node.printAllNodes();
+        boolean b = hasCycle(head);
+        System.out.println(b);
+    }
+
+    static boolean hasCycle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static ListNode removeNode(ListNode head, int n) {
+        if (head == null || n == 0) {
+            return null;
+        }
         ListNode pre = new ListNode(0, head);
-        ListNode fast = head, slow = pre;
+        ListNode fast = pre, slow = pre;
         while (n != 0) {
             fast = fast.next;
             n--;
